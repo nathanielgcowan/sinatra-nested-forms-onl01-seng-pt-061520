6ed44393e5b07b0ca6ffa1f8fb8d762cf60cf78f
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-10-22 12:49:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require './environment'
 
 module FormsLab
@@ -23,3 +24,32 @@ module FormsLab
     
   end
 end
+=======
+require './environment'
+
+module FormsLab
+  class App < Sinatra::Base
+
+    # code other routes/actions here
+    get '/' do 
+      erb :root
+    end
+    
+    get '/new' do 
+      erb: 'pirates/new'
+    end 
+    
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      
+      params[:pirate][:ships.each do |details]
+      Ship.new(details)
+      end 
+      @ships = Ship.all
+      
+      erb : 'pirates/show'
+    end 
+    
+    
+end
+>>>>>>> a3107fd8b0c3a9b11caa29feb80a3e9ca7aee52d
